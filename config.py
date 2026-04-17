82ce12a50e9da2c3d4d42bd432e97c895bc19fe4
LostWarrior <kbhardwaj@gocardless.com>
2026-04-17 18:10:40 +0100

    increase context window to 512

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ vocab_size = 4000
 embed_dim = 384
 num_heads = 8
 num_layers = 10
-max_seq_len = 256
+max_seq_len = 512
 dropout = 0.1
 
 # Training
 batch_size = 16
 learning_rate = 3e-4
-max_steps = 10000
+max_steps = 20000
 eval_interval = 1000
